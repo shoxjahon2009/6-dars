64b1c70273fb29f856836ea5fadc04ad4afcad64
Пользователь <shohjaxondehqonboyev59@gmail.com>
2023-12-07 15:44:55 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+#  names = ["ali" , "vali" ,"azamat" ,"shox"]
+# for name in names:
+#     if name == "ali":
+#         print(f"{name} salom")
+#     elif name == "vali" :
+#         print(f"{name} salom")
+#     elif name == "azamat"  :
+#         print(f"{name} salom")
+
+
+
+# while True:
+#     a = str(input("1-soni kirgizing"))
+#     b = str(input("2-soni kiriting"))
+#     c = str(input("3-soni kiriting"))
+#     d = str(input("4-soni kiriting"))
+#
+#     name = [a, b, c, d]
+#     for m in name:
+#         print(f"{m} qonday sonlar tinchmi")
+
+
+# names = ["abdulloh", "muhammad", "husniddin"]
+# n = input("ism kiriting")
+# x = names.count("")
+# for name in names:
+#     if name == n:
+#         print(f"{name} bilan suhbad qildingiz ")
