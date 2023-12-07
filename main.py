79e18bfa643bee439722a53737173c2b03c02434
Пользователь <shohjaxondehqonboyev59@gmail.com>
2023-12-07 16:27:06 +0500

    first commit

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+# 1-misol
 
 
-#  names = ["ali" , "vali" ,"azamat" ,"shox"]
-# for name in names:
-#     if name == "ali":
-#         print(f"{name} salom")
-#     elif name == "vali" :
-#         print(f"{name} salom")
-#     elif name == "azamat"  :
-#         print(f"{name} salom")
-
-
-
-# while True:
-#     a = str(input("1-soni kirgizing"))
-#     b = str(input("2-soni kiriting"))
-#     c = str(input("3-soni kiriting"))
-#     d = str(input("4-soni kiriting"))
+# a = str(input("1-ismni kiriting:"))
+# b = str(input("2-ismni kiriting:"))
+# c = str(input("3-ismni kiriting:"))
+# d = str(input("4-ismni kiriting:"))
+# e = str(input("5-ismni kiriting:"))
 #
-#     name = [a, b, c, d]
-#     for m in name:
-#         print(f"{m} qonday sonlar tinchmi")
+# names = [a, b, c, d, e]
+#
+# for s in names:
+#     print(f"Salom {s}!!!")
 
+
+
+# 2-misol
+
+# a = int(input("1-soni kirgizing"))
+# b = int(input("2-soni kiriting"))
+# c = int(input("3-soni kiriting"))
+# d = (int1
+#      (input("4-soni kiriting")))
+#
+# name = [a, b, c, d]
+# for m in name:
+#     print(f"sizning javobingiz {m ** 3}")
+
+# 3-misol
 
 # names = ["abdulloh", "muhammad", "husniddin"]
 # n = input("ism kiriting")
